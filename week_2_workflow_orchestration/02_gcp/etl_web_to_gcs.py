61ee41299f501d265d738c747f6701b9d80a6539
Kirill Bazarov <kl.bazarov@gmail.com>
2023-12-03 07:52:56 -0300

    week2: etl_gcs_bq

---
+++ week_2_workflow_orchestration/02_gcp/etl_web_to_gcs.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas Dataframe"""
+
+    df = pd.read_csv(dataset_url)
+    return df
+
+
+@task(log_prints=True)
+def clean(df = pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@flow()
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write Dataframe out locally as parquet file"""
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+    return path
+
+
+@flow()
+def write_gsc(path: Path) -> None:
+    """Upload local parquet file to GSC"""
+    gsc_block = GcsBucket.load("de-zoomcamp-gsc")
+    gsc_block.upload_from_path(
+        from_path = f"{path}",
+        to_path = path
+    )
+    return
+
+
+@flow()
+def etl_web_to_gsc() -> None:
+    """The main ETL function"""
+    color = "yellow"
+    year = 2021
+    month = 1
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+
+    write_gsc(path)
+
+if __name__ == '__main__':
+    etl_web_to_gsc()
